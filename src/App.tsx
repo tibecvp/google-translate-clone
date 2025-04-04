205dfa02d0ab9b366c2d339066427bfb9126ef34
Tibe Calderon <tibecvp@gmail.com>
2025-04-03 22:01:52 -0400

    Creation of a Custom Hook for useStore

--- src/App.tsx
+++ src/App.tsx
@@ -1,76 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
-import { useReducer } from 'react'
-import { Action, type State } from './types.d'
-
-const initialState: State = {
-  sourceLanguage: 'auto',
-  targetLanguage: 'es',
-  sourceText: '',
-  translatedText: '',
-  loading: false,
-}
-
-function reducer(state: State, action: Action) {
-  const { type } = action
-
-  if (type === 'INTERCHANGE_LANGUAGES') {
-    return {
-      ...state,
-      sourceLanguage: state.targetLanguage,
-      targetLanguage: state.sourceLanguage,
-    }
-  }
-
-  if (type === 'SET_SOURCE_LANGUAGE') {
-    return {
-      ...state,
-      sourceLanguage: action.payload
-    }
-  }
-
-  if (type === 'SET_TARGET_LANGUAGE') {
-    return {
-      ...state,
-      targetLanguage: action.payload
-    }
-  }
-
-  if (type === 'SET_SOURCE_TEXT') {
-    return {
-      ...state,
-      loading: true,
-      sourceText: action.payload,
-      translatedText: ''
-    }
-  }
-
-  if (type === 'SET_TRANSLATED_TEXT') {
-    return {
-      ...state,
-      loading: false,
-      translatedText: action.payload
-    }
-  }
-
-  return state
-}
+import { useStore } from './hooks/useStore'
 
 function App() {
-  const [{
-    sourceLanguage,
-    targetLanguage,
-    sourceText,
-    translatedText,
-    loading
-  }, dispatch] = useReducer(reducer, initialState)
-
-  console.log('sourceLanguage', sourceLanguage)
+  const { sourceLanguage, setSourceLanguage } = useStore()
   return (
     <div className='App'>
       <h1>Google Tanslate by Tibecvp</h1>
       <button onClick={() => {
-        dispatch({ type: 'SET_SOURCE_LANGUAGE', payload: 'en' })
+        setSourceLanguage('en')
       }}>Set source language to English</button>
       {sourceLanguage}
     </div>
---
+++ src/hooks/useStore.ts
@@ -0,0 +1,97 @@
+import { useReducer } from 'react'
+import { Action, type State } from '../types.d'
+
+const initialState: State = {
+    sourceLanguage: 'auto',
+    targetLanguage: 'es',
+    sourceText: '',
+    translatedText: '',
+    loading: false,
+  }
+  
+  function reducer(state: State, action: Action) {
+    const { type } = action
+  
+    if (type === 'INTERCHANGE_LANGUAGES') {
+      return {
+        ...state,
+        sourceLanguage: state.targetLanguage,
+        targetLanguage: state.sourceLanguage,
+      }
+    }
+  
+    if (type === 'SET_SOURCE_LANGUAGE') {
+      return {
+        ...state,
+        sourceLanguage: action.payload
+      }
+    }
+  
+    if (type === 'SET_TARGET_LANGUAGE') {
+      return {
+        ...state,
+        targetLanguage: action.payload
+      }
+    }
+  
+    if (type === 'SET_SOURCE_TEXT') {
+      return {
+        ...state,
+        loading: true,
+        sourceText: action.payload,
+        translatedText: ''
+      }
+    }
+  
+    if (type === 'SET_TRANSLATED_TEXT') {
+      return {
+        ...state,
+        loading: false,
+        translatedText: action.payload
+      }
+    }
+  
+    return state
+  }
+
+  export function useStore () {
+    const [{
+      sourceLanguage,
+      targetLanguage,
+      sourceText,
+      translatedText,
+      loading
+    }, dispatch] = useReducer(reducer, initialState)
+
+    const interchangeLanguages = () => {
+      dispatch({ type: 'INTERCHANGE_LANGUAGES' })
+    }
+
+    const setSourceLanguage = (payload: string) => {
+      dispatch({ type: 'SET_SOURCE_LANGUAGE', payload })
+    }
+
+    const setTargetLanguage = (payload: string) => {
+      dispatch({ type: 'SET_TARGET_LANGUAGE', payload })
+    }
+
+    const setSourceText = (payload: string) => {
+      dispatch({ type: 'SET_SOURCE_TEXT', payload})
+    }
+    const setTranslatedText = (payload: string) => {
+      dispatch({ type: 'SET_TRANSLATED_TEXT', payload })
+    }
+
+    return {
+      sourceLanguage,
+      targetLanguage,
+      sourceText,
+      translatedText,
+      loading,
+      interchangeLanguages,
+      setSourceLanguage,
+      setTargetLanguage,
+      setSourceText,
+      setTranslatedText
+    }
+  }
